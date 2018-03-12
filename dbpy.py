0bb6db161567993a2e6d5c322fc6045d31d69382
Shizen39 <mazzagiorgio96@gmail.com>
2018-03-12 19:33:03 +0100

    Modifica getPlant + getHerbivore

--- dbpy.py
+++ dbpy.py
@@ -17,16 +17,51 @@ class dbInterface(Datainterface):
         cur.execute("select * from World.terreno as x where x.codice= %s ;" ,(code, ))
         return cur.fetchone()
     
+    def searchCode(self, code, cur):
+        pass
+    #NON SIAMO CAPACI A CHIAMARE UNA FUNZIONE DENTRO AD UN'ALTRA
     def getPlant(self,code):
         cur = self.getConnect()
-        cur.execute("select * from World.piante join World.comportamento on World.comportamento.id=World.piante.comp;")
+        cur.execute("select * from World.piante join World.comportamento "+
+        "on World.comportamento.id=World.piante.comp;")
+        b=None
         for a in cur:
-            if ( a == None):
-                return "fine"
-        return a    
+            if (code!=a[0]):
+                continue  
+            else:
+                b=a
+                break
+        if (b==None):
+            return "fine"
+        else:
+            return b
+    
+    def getHerbivore(self, code):
+        cur = self.getConnect()
+        cur.execute("select * from World.erbivori join World.comportamento "+
+        "on World.comportamento.id=World.erbivori.comp;")
+        b=None
+        for a in cur:
+            if (code!=a[0]):
+                continue  
+            else:
+                b=a
+                break
+        if (b==None):
+            return "fine"
+        else:
+            return b
+
+    def getCarnivorous(self, code):
+        pass
+    def getOmnivorous(self, code):
+        pass
 
 db=dbInterface()
+
 print(db.getPlant(3))
+    
+print(db.getHerbivore(2))
 
 
 
