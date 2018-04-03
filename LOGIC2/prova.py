198d6f27d377412a8dbedfed7a62447be0fd22d9
Filippo <filippo.ufficiale@gmail.com>
2018-04-03 12:44:55 +0200

    continuo stampa

--- LOGIC2/prova.py
+++ LOGIC2/prova.py
@@ -1,55 +1,45 @@
 from Xel import *
 
 radius=5
-ra=6
-org=[0,1]
-org[0]= Exa()
-org[1]= Xel()
-
-org[1].newHex(radius)
+org= Exa()
+rorg= Xel()
+rorg.newHex(radius)
+while rorg.link["a"]!= None:
+    rorg=rorg.link["a"]
 
 result=""
 
 def menu(org):
-    print("origine" + str(org[1].exa))
-    print("posizione" + str(org[0]))
     a=input()
     if a=="q":
-        org[0]=org[0].Q()
-        org[1]=org[1].link[a]
+        org=org.Q()
     if a=="w":
-        org[0]=org[0].W()
-        org[1]=org[1].link[a]
+        org=org.W()
     if a=="e":
-        org[0]=org[0].E()
-        org[1]=org[1].link[a]
+        org=org.E()
     if a=="d":
-        org[0]=org[0].D()
-        org[1]=org[1].link[a]
+        org=org.D()
     if a=="s":
-         org[0]=org[0].S()
-         org[1]=org[1].link[a]
+         org=org.S()
     if a=="a":
-        org[0]=org[0].A()
-        org[1]=org[1].link[a]
-    return [org[0], org[1]]
+        org=org.A()
+    return org
 
 while True :
-    a=ra
     result=""
+    temorg = rorg
     for i in range(-radius, radius+1):
         for j in range(abs(i)):
             result += " "
-        for j in range(a):
-            if org[1].exa == org[0] :
+        while temorg.link["w"]!= None:
+            if temorg.exa == org :
                 result += "o "
             else:
                 result += ". "
-        if i < 0:
-            a += 1
-        else:
-            a -= 1
+            temorg=temorg.link["w"]
         result += "\n"
+        if i<0:
+
     print(result)
     org=menu(org)
 
