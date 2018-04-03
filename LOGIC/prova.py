a9644082a64f73c455a296b63d46391fcc7d66ce
Filippo <filippo.ufficiale@gmail.com>
2018-04-03 16:04:21 +0200

    fine prova, è scomodo ma funge.

--- LOGIC/prova.py
+++ LOGIC/prova.py
@@ -1,46 +1,63 @@
 from Xel import *
 
-radius=5
+radius=3
 org= Exa()
-rorg= Xel()
-rorg.newHex(radius)
-while rorg.link["a"]!= None:
-    rorg=rorg.link["a"]
+origin=Xel.newHex(radius)
+origin
+while origin.link["a"]!= None:
+    origin=origin.link["a"] 
 
 result=""
 
 def menu(org):
     a=input()
     if a=="q":
-        org=org.Q()
-    if a=="w":
-        org=org.W()
-    if a=="e":
-        org=org.E()
-    if a=="d":
-        org=org.D()
-    if a=="s":
-         org=org.S()
-    if a=="a":
         org=org.A()
+    if a=="w":
+        org=org.Q()
+    if a=="a":
+        org=org.S()
+    if a=="s":
+        org=org.W()
+    if a=="z":
+        org=org.D()
+    if a=="x":
+        org=org.E()
+    if a=="exit":
+        quit()
     return org
 
 while True :
+    print("""istruzioni:
+    ci si muove con q,w,a,s,z,x, purtroppo essendo l'esagono piegato di novanta gradi ho dovuto improvvisare,
+    si esce scrivendo 'exit' senza apostrofi, buon divertimento""")
+    rorg=origin
     result=""
-    temorg = rorg
     for i in range(-radius, radius+1):
+        temorg = rorg
         for j in range(abs(i)):
             result += " "
-        while temorg.link["w"]!= None:
+
+        if org.e > radius or org.x > radius or org.a > radius :
+            print("\nout of range\n")
+            break
+
+        while temorg != None:
             if temorg.exa == org :
                 result += "o "
+
             else:
                 result += ". "
+
             temorg=temorg.link["w"]
         result += "\n"
         if i<0:
-
+            rorg=rorg.link["d"]
+        else:
+            rorg=rorg.link["e"]
+        #print("temorg=",temorg,"\nrorg=",rorg,"\n\n")
     print(result)
+    print("position:",org)
     org=menu(org)
 
 
