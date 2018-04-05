6dba283c3d640ddd1af4c3b9163593eb60d46af4
frabulous <francesco.biulf@gmail.com>
2018-04-05 14:31:29 +0200

    bozza di esagoni con Pygame e OpenGL

---
+++ GRAPHIC/GraphicHexagon.py
@@ -0,0 +1,80 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import math
+
+
+class GraphicHexagon:
+
+    def __init__(self, center, size,
+                 pointy_top=False, filled=False,
+                 color=(1.0, 1.0, 1.0, 1.0),
+                 line_thickness=1):
+
+        self.__center = center
+        self.__size = size
+        self.__pointy_top = bool(pointy_top)
+        self.__filled = bool(filled)
+        self.__color = color
+        self.__line_thickness = line_thickness
+
+    def set_center(self, new_center):
+        self.__center = new_center
+
+    def get_center(self):
+        return self.__center
+
+    def set_size(self, new_size):
+        self.__size = new_size
+
+    def get_size(self):
+        return self.__size
+
+    def set_pointy_top(self, new_pointy_top):
+        self.__pointy_top = new_pointy_top
+
+    def get_pointy_top(self):
+        return self.__pointy_top
+
+    def set_filled(self, new_filled):
+        self.__filled = bool(new_filled)
+
+    def get_filled(self):
+        return self._filled
+
+    def set_color(self, color):
+        self.__color = color
+
+    def get_color(self, color):
+        return self.__color
+
+    def set_line_thickness(self, new_line_thickness):
+        self.__line_thickness = new_line_thickness
+
+    def get_line_thickness(self):
+        return self.line_thickness
+
+    def draw(self):
+        glLoadIdentity()
+
+        if self.__filled:
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+        else:
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+        glColor4fv(self.__color)
+        glLineWidth(self.__line_thickness)
+
+        glBegin(GL_POLYGON)
+
+        for i in range(6):
+
+            if self.__pointy_top:
+                angle = (60 / 180.0) * math.pi * i
+            else:
+                angle = ((30 / 180.0) * math.pi) + (60 / 180.0) * math.pi * i
+
+            glVertex2d(self.__center[0] +
+                       self.__size * math.sin(angle),
+                       self.__center[1] +
+                       self.__size * math.cos(angle))
+        glEnd()
---
+++ GRAPHIC/provaHexagon.py
@@ -0,0 +1,67 @@
+import math
+import pygame as pg
+from GraphicHexagon import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+# ----------------------------------------------------------
+# Entry Point
+# ----------------------------------------------------------
+if __name__ == "__main__":
+    # TODO trovare il centro della finestra di pygame
+    # TODO trovare il modo di sommare le coppie di coordinate ES: hex.center + (x,y)
+
+    # Pygame initialization
+    pg.init()
+    displayInfo = pg.display.Info()
+    window_width = 800
+    window_height = 640
+    hex_side = 40
+    gameDisplay = pg.display.set_mode((window_width, window_height), pg.OPENGL | pg.DOUBLEBUF)
+    pg.display.set_caption("Prova 1")
+
+    # OpenGL initialization
+    glClearColor(0.0, 0.0, 0.0, 1.0)
+    glViewport(0, 0, 640, 480)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(0, 640, 480, 0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    glEnable(GL_POINT_SMOOTH)
+    glEnable(GL_LINE_SMOOTH)
+
+    # Origin Hexagon center = center of the pygame window
+    origin_center = (gameDisplay.get_width()/2, gameDisplay.get_height()/2)
+
+    # Creating hexagons
+    hex0 = GraphicHexagon(origin_center, hex_side)
+    """
+    hex1 = Hexagon((100, 100), 20, False)
+    hex2 = Hexagon((200, 200), 40, True)
+    hex3 = Hexagon((300, 300), 40, True, False, (1.0, 0.0, 0.0, 1.0))
+    hex4 = Hexagon((100, 300), 30, True, False, (1.0, 0.0, 0.0, 1.0), 3)
+    hex5 = Hexagon((300, 100), 30, False, True, (1.0, 0.0, 0.0, 1.0), 5)
+    """
+    center1 = (gameDisplay.get_width()/2, gameDisplay.get_height()/2 + hex_side*math.sqrt(3))
+    hex1 = GraphicHexagon(center1, hex_side)
+
+    # Events loop
+    clock = pg.time.Clock()
+    while True:
+        event = pg.event.poll()
+        if event.type == pg.QUIT:
+            pg.quit()
+            break
+
+        # Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # Draw the hexagons
+        hex0.draw()
+        hex1.draw()
+
+        # Update
+        pg.display.flip()
+
+    clock.tick(30)
