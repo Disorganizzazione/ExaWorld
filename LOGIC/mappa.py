281da7b157ea0b77d3636dcdc9519e7553d969f4
Filippo <filippo.ufficiale@gmail.com>
2018-04-06 16:59:38 +0200

    inizio mappa

--- LOGIC/mappa.py
+++ LOGIC/mappa.py
@@ -1,70 +1,79 @@
 from Xel import *
-radius=100
-g_map=None
-g_position=None
-l_map=[]
-l_position=[]
-b_map={'qw':None,'we':None, 'ed':None,'ds':None,'sa':None,'aq':None}
+radius=10#radius maps
+g_map=None#pointer to origin of the global map
+g_position=None #global position
+l_map=list()#map where your are in (max 3)
+l_position=list()#your position in l_map (max 3)
+b_map={'qw':None,'we':None, 'ed':None,'ds':None,'sa':None,'aq':None}#map stored in memory
 
-def map():
+def init():
     g_map=Xel.newHex(radius)
     g_position=Exa()
     l_map.append(Xel.newHex(radius))
+    l_position.append(Exa())
+    for i in b_map :
+        b_map[i]=Xel.newHex(radius)
+
+def mirror() :
+    print("ciao")
+    if l_position[0].e == radius or l_position[0].e == -radius:
+        l_position.append(Exa(-l_position[0].e,-l_position[0].a,-l_position[0].x))
+        print(l_position[1],"primo")
+    if l_position[0].x == radius or l_position[0].x == -radius:
+        l_position.append(Exa(-l_position[0].a,-l_position[0].x,-l_position[0].e))
+        print(l_position[2],"secondo")
+    if l_position[0].a == radius or l_position[0].a == -radius:
+        l_position.append(Exa(-l_position[0].e,-l_position[0].a,-l_position[0].x))
 
 def cross_map():
-    l_position=mirror(l_position[0])
-    if l_position.len()>0:
+    mirror()
+    if l_position.__len__()>0:
         if abs(l_position[0].e)==radius:
             if l_position[0].e<0:
-                l_map.append(b_map('ds'))
+                l_map.append(b_map['ds'])
             else:
-                l_map.append(b_map('qw'))
+                l_map.append(b_map['qw'])
         if abs(l_position[0].x)==radius:
             if l_position[0].x<0:
-                l_map.append(b_map('aq'))
+                l_map.append(b_map['aq'])
             else:
-                l_map.append(b_map('ed'))
+                l_map.append(b_map['ed'])
         if abs(l_position[0].a)==radius:
             if l_position[0].a<0:
-                 l_map.append(b_map('we'))
+                 l_map.append(b_map['we'])
             else:
-                l_map.append(b_map('sa'))
+                l_map.append(b_map['sa'])
 
-
-
-
-def mirror(position) :
-    pos=[position]
-    if position.e == radius or position.e == -radius:
-        pos.append(Exa(-position.e,-position.a,-position.x))
-    if position.x == radius or position.x == -radius:
-        pos.append(Exa(-position.e,-position.a,-position.x))
-    if position.a == radius or position.a == -radius:
-        pos.append(Exa(-position.e,-position.a,-position.x))
-    return pos
-
-def menu(org):
+def menu():
     a=input()
     if a=="q":
-        lposition[0]=l_position[0].A()
+        l_position[0]=l_position[0].Q()
         cross_map()
     if a=="w":
-        lposition[0]=l_position[0].Q()
+        l_position[0]=l_position[0].W()
         cross_map()
-    if a=="a":
-        lposition[0]=l_position[0].S()
+    if a=="e":
+        l_position[0]=l_position[0].E()
+        cross_map()
+    if a=="d":
+        l_position[0]=l_position[0].D()
         cross_map()
     if a=="s":
-        lposition[0]=l_position[0].W()
+        l_position[0]=l_position[0].S()
         cross_map()
-    if a=="z":
-        lposition[0]=l_position[0].D()
-        cross_map()
-    if a=="x":
-        lposition[0]=l_position[0].E()
+    if a=="a":
+        l_position[0]=l_position[0].A()
         cross_map()
     if a=="exit":
         quit()
-    return org
 
-map
+
+init()
+print(l_position[0])
+while 1:
+    menu()
+    print("--------------------------------------------------------------------------------")
+    try:
+        print(l_position[0],'mappa2',l_position[1],'mappa3', l_position[2])
+    except :
+        print(l_map[0],l_position[0])
