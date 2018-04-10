b3effabc84597a790ef8d448deafe776b5773296
frabulous <francesco.biulf@gmail.com>
2018-04-10 14:41:02 +0200

    prova from Exa to Pixel

--- GRAPHIC/provaHexagon.py
+++ GRAPHIC/provaHexagon.py
@@ -1,8 +1,9 @@
+
 import math
 import pygame as pg
-from GraphicHexagon import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from GraphicHexagon import *
 
 # ----------------------------------------------------------
 # Entry Point
@@ -12,27 +13,26 @@ if __name__ == "__main__":
     # Pygame initialization
     pg.init()
     displayInfo = pg.display.Info()
-    window_width = 800
-    window_height = 640
-    side = 40
-    hex_side = side - 5
-    gameDisplay = pg.display.set_mode((window_width, window_height), pg.OPENGL | pg.DOUBLEBUF)
+    window_width = displayInfo.current_w - 50
+    window_height = displayInfo.current_h - 50
+    side = 30
+    hex_side = math.floor(side*9/10)
+    gameDisplay = pg.display.set_mode((window_width, window_height), pg.OPENGL | pg.DOUBLEBUF | pg.OPENGLBLIT)
     pg.display.set_caption("Prova 1")
 
     # OpenGL initialization
     glClearColor(0.0, 0.0, 0.0, 1.0)
-    glViewport(0, 0, 640, 480)
+    glViewport(0, 0, window_width, window_height)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluOrtho2D(0, 640, 480, 0)
+    gluOrtho2D(0, window_width, window_height, 0)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
     glEnable(GL_POINT_SMOOTH)
     glEnable(GL_LINE_SMOOTH)
 
     # Origin Hexagon center = center of the pygame window
-    # TODO: capire perché height/4 e non height/2
-    origin_center = (gameDisplay.get_width()/2, gameDisplay.get_height()/4)
+    origin_center = (gameDisplay.get_width()/2, gameDisplay.get_height()/2)
 
     v3s = math.sqrt(3) * side / 2
     s3 = 3 * side / 2
@@ -54,6 +54,22 @@ if __name__ == "__main__":
 
         hexs.append(GraphicHexagon((center[0], center[1]), hex_side))
 
+    # Prova: da Exa a GraphicHexagon
+    # exo = Exa(2,2,-4)
+    exo = (2,2,-4)
+    # xello = Xel(exo)
+    x, y = hex0.get_center()
+    x = x + side*3/2*exo[1]
+    y = y + side*math.sqrt(3)*(exo[2] + exo[1]/2)
+    hexago = GraphicHexagon((x, y), hex_side)
+
+    # initialize font; must be called after 'pygame.init()' to avoid 'Font not Initialized' error
+    myfont = pg.font.SysFont("monospace", 30)
+    # render text
+    #label = myfont.render("Some text!", True, (255, 255, 255))
+    #gameDisplay.blit(label, hexago.get_center())
+
+
     # Events loop
     clock = pg.time.Clock()
     while True:
@@ -69,6 +85,10 @@ if __name__ == "__main__":
         hex0.draw()
         for h in hexs:
             h.draw()
+        hexago.draw()
+        label = myfont.render("Some text!", True, (255, 255, 0))
+        gameDisplay.blit(label, hexago.get_center())
+        print(label)
 
         # Update
         pg.display.flip()
