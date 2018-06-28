fe2958f94f541608d550adc708d27ee12b40b67f
Shizen39 <mazzagiorgio96@gmail.com>
2018-06-28 14:46:56 +0200

    Light

--- GRAPHIC/PandaProva.py
+++ GRAPHIC/PandaProva.py
@@ -1,4 +1,9 @@
 from direct.showbase.ShowBase import ShowBase
+from panda3d.core import PointLight
+from panda3d.core import DirectionalLight
+from panda3d.core import AmbientLight
+from panda3d.core import VBase4
+
 
 class MyApp(ShowBase):
     def __init__(self):
@@ -8,6 +13,20 @@ class MyApp(ShowBase):
 
         self.obj1.setPos(-3,7,5)
         self.useDrive()
+        self.setupLight()
+
+    def setupLight(self):
+        primeL= DirectionalLight("prime")
+        primeL.setColor(VBase4(.6,.6,.6,1))
+        self.light= render.attachNewNode(primeL)
+        self.light.setHpr(45,-60,0)
+        render.setLight(self.light)
+        
+        ambL= AmbientLight("amb")
+        ambL.setColor(VBase4(.2,.2,.2,1))
+        self.ambLight= render.attachNewNode(ambL)
+        render.setLight(self.ambLight)
+        return
 
 app = MyApp()
 app.run()
