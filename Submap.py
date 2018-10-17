be3ad983cc93ff55ca4f5720e86f03fa4e96d675
Shizen39 <mazzagiorgio96@gmail.com>
2018-10-17 11:31:46 +0200

    0.9+21.7=22.5999999998? Removed rounds and added isclose()

--- Submap.py
+++ Submap.py
@@ -2,7 +2,7 @@ from LOGIC import Xel
 
 class Submap:
     def __init__(self, centerXY, array_Z=[0,0,0,0,0,0,0], array_T=[0,0,0,0,0,0,0], array_H=[0,0,0,0,0,0,0], noise_seed=0):
-        self.centerXY = centerXY
+        self.centerXY = (centerXY[0].__round__(1), centerXY[1].__round__(1))
         self.array_Z = array_Z
         self.array_T = array_T
         self.array_H = array_H
