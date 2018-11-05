a160bd83501f88c849e3073c2c34a650601c2e80
Grushnack <37400932+Grushnack@users.noreply.github.com>
2018-11-05 11:23:12 +0100

    debug
    debug deploy

--- CONCEPT/Deploy.py
+++ CONCEPT/Deploy.py
@@ -14,32 +14,34 @@ class content():
         a=1
         for i in db.creatures:
             creatures=DBclass.Creature(db,a)
-
             if creatures.tipo== "vegetale":
                 if temp <= creatures.maxtemp and temp >= creatures.mintemp  and umid <= creatures.maxhmd and umid >= creatures.minhmd:
                     vegetal.append(creatures)
             else:
                 if temp <= creatures.maxtemp and temp >= creatures.mintemp  and umid <= creatures.maxhmd and umid >= creatures.minhmd:
                     animal.append(creatures)
-
             a=a+1
 
         animal.append(None)
         vegetal.append(None)
         a=1
         for i in db.terrains:
-            terrains=DBclass.Terrain(db,a)
-            if temp <= creatures.maxtemp and temp >= creatures.mintemp  and umid <= creatures.maxhmd and umid >= creatures.minhmd:
-                terrain.append(terrains)
+            terr=DBclass.Terrain(db,a)
+            if temp <= terr.maxtemp and temp >= terr.mintemp  and umid <= terr.maxhmd and umid >= terr.minhmd:
+                terrain.append(terr)
             a=a+1
 
         terrain.append(None)
+
+        print(vegetal)
+        print(animal)
+        print(terrain)
+        print("\n\n")
 
 
         indexa=random.randint(0,len(animal)-1)
         indexv=random.randint(0,len(vegetal)-1)
         indext=random.randint(0,len(terrain)-1)
-
         if gxel not in self.content.keys() :
             self.content[gxel]={}
 
@@ -51,7 +53,6 @@ a=content()
 a.creation((0,0,0),(1,3,2),20,20)
 a.creation((0,0,0),(1,4,2),20,20)
 a.creation((0,1,0),(1,3,2),20,20)
-
 print(a.content)
 
 
