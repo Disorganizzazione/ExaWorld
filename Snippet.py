a0b1063aa28efab5cad1e41b83a3e82780718bd2
Grushnack <37400932+Grushnack@users.noreply.github.com>
2018-11-06 11:58:41 +0100

    Update Snippet.py
    Tolte righe di debug, aggiunti commenti alle funzioni

--- Snippet.py
+++ Snippet.py
@@ -1,34 +1,21 @@
 import json
 import os
 
-def save(dictionary):
-    if not os.path.exists("json_files"):
+def save(dictionary):                   #funzione che salva su un file json un dizionario
+    if not os.path.exists("json_files"):    #crea la cartella se non esiste
         os.makedirs("json_files")
-    #with open("json_files/coordinates.json", "w") as fp:
-        #json.dump(dictionary, fp)
     data = open("json_files/coordinates.json", "w")
     data.write("{ ")
     keys = list(dictionary.keys())
     values = list(dictionary.values())
-    for i in range(0,len(dictionary)-2):
+    for i in range(0,len(dictionary)-2):        #scrivi il dizionario in sintassi json
         data.write('"' + keys[i] + '"' + ':' + '"' + str(values[i]) + '"')
         data.write(', ')
     data.write('"' + keys[len(dictionary)-1] + '"' + ':' + '"' + str(values[len(dictionary)-1]) + '"')
-    data.write("}")
+    data.write("}")                             #niente virgola sull'ultimo elemento
     data.close()
 
-def load(filepath):
+def load(filepath):             #funzione che carica un file json da un percorso
     with open(filepath, "r") as fp:
         obj = json.load(fp)
         return(obj)
-
-data = dict([
-    ("terrain","forest"),
-    ("creature","rabbit"),
-    ("strength",6),
-    ("type","a")
-])
-save(data)
-obj = load("json_files/coordinates.json")
-print(obj["terrain"])
-
